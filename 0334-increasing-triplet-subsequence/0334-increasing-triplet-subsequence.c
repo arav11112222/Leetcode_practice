8dbc3152937269a579f6af1e1920e0c9f23fd103
Aarav <aravgupta1412@gmail.com>
2026-09-20 23:13:33 +0530

    Time: 1 ms (36.25%), Space: 27 MB (42.08%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.c
@@ -0,0 +1,14 @@
+bool increasingTriplet(int* nums, int n) {
+    int a = INT_MAX, b = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] <= a)
+            a = nums[i];
+        else if (nums[i] <= b)
+            b = nums[i];
+        else
+            return true;
+    }
+
+    return false;
+}
